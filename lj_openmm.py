bd754842b16ff7bae4dadb5b848454eae818538a
daviddesancho <daviddesancho@gmail.com>
2024-11-22 17:33:38 +0100

    equilibrates single particle in NPT

--- lj_openmm.py
+++ lj_openmm.py
@@ -1,10 +1,11 @@
+
+import numpy as np
 import sys, yaml
 
 import openmm as mm
 from openmm import app
 from openmm import unit
 from openmm.app.pdbfile import PDBFile
-import numpy as np
 
 class Sim(object):
     def __init__(self):
@@ -54,22 +55,15 @@ class Sim(object):
         for k,v in self.components.items():
             n_particles += v['nmol'] 
             for _ in range(v['nmol']):
-                system.addParticle(1.0 * unit.dalton)  # Assign equal masses to all particles
-
-        # Generate random positions
-        box_size = self.box
-        min_box_size = np.min(box_size)
-        positions = np.random.uniform(low=-min_box_size/2, high=min_box_size/2, \
-                                      size=(n_particles, 3))
+                system.addParticle(v['mass'] * unit.dalton)  
         print("# Number of particles in system:", system.getNumParticles())
-        print("# Number of positions provided:", len(positions))
 
         # Define periodic box vectors
+        box_size = self.box
         box_vectors = mm.Vec3(box_size[0] * unit.nanometer, 0, 0),\
                     mm.Vec3(0, box_size[1] * unit.nanometer, 0), \
                     mm.Vec3(0, 0, box_size[2] * unit.nanometer)
         system.setDefaultPeriodicBoxVectors(*box_vectors)
-
         self.system = system
 
     def make_topology(self):
@@ -114,7 +108,7 @@ class Sim(object):
         # Create a nonbonded force for Lennard-Jones interactions
         nonbonded = mm.NonbondedForce()
         nonbonded.setNonbondedMethod(mm.NonbondedForce.CutoffPeriodic)
-        nonbonded.setCutoffDistance(1.2 * unit.nanometer)
+        nonbonded.setCutoffDistance(1. * unit.nanometer)
         nonbonded.setUseDispersionCorrection(True)
 
         # Assign Lennard-Jones parameters
@@ -137,13 +131,16 @@ class Sim(object):
         """
         # Simulation setup
         try:
-            platform = mm.Platform.getPlatformByName('CPU')  
+            platform = mm.Platform.getPlatformByName('CUDA')  
             print ("# Running on GPU") 
         except Exception as e:
             platform = mm.Platform.getPlatformByName('CPU')
             print ("# CUDA Platform not available; running on CPU") 
 
-        integrator = mm.LangevinIntegrator(self.temperature, 1/unit.picosecond, self.timestep)
+        self.system.addForce(mm.MonteCarloBarostat(self.pressure * unit.bar,\
+                                    self.temperature * unit.kelvin))
+        integrator = mm.LangevinIntegrator(self.temperature * unit.kelvin, \
+                                    1/unit.picosecond, self.timestep)
         simulation = app.Simulation(self.topology, self.system, integrator, platform)
 
         # Set initial positions
@@ -158,7 +155,23 @@ class Sim(object):
 
     def run(self):
         """
+
         """
         # Minimize energy
         print("Minimizing energy...")
         self.simulation.minimizeEnergy()
+
+        positions = self.simulation.context.getState(getPositions=True).getPositions()
+        with open("minimized.pdb", "w") as f:
+            PDBFile.writeFile(self.simulation.topology, positions, f)
+
+        self.simulation.reporters.append(app.StateDataReporter("equilibration.csv", 100, \
+                                step=True, potentialEnergy=True, temperature=True, \
+                                    density=True))
+        self.simulation.reporters.append(app.DCDReporter('equilibration.dcd', 100))
+
+        # Equilibrate
+        self.simulation.context.setVelocitiesToTemperature(self.temperature)
+        self.simulation.context.setPositions(positions)
+        print("Equilibrating...")
+        self.simulation.step(1000000)
