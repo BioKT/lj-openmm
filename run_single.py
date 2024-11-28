06e8a57fccf59afc9458f73c779f03a06bdebacc
David <daviddesancho@gmail.com>
2024-11-28 16:26:46 +0100

    adds slab analysis

--- run_single.py
+++ run_single.py
@@ -1,16 +1,17 @@
 from openmm import unit
 import lj_openmm
 
-simulation = lj_openmm.Sim()
+for i in ["argon", "krypton", "xenon"]:
+    simulation = lj_openmm.Sim()
 
-simulation.read_input("single.yml")
+    simulation.read_input("data/%s_slab.yml"%i)
 
-simulation.make_system()
+    simulation.make_system()
 
-simulation.make_topology()
+    simulation.make_topology()
 
-simulation.force_field()
+    simulation.force_field()
 
-simulation.make_simulation()
+    simulation.make_simulation()
 
-simulation.run()
+    simulation.run()
